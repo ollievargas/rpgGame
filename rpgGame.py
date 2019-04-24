bc8aa2d2f4ae3d67c338d678253e263d75843c93
Ollie Vargas <ojvargas_76@hotmail.com>
2019-04-24 13:46:30 -0500

    rpgGame

---
+++ rpgGame.py
@@ -0,0 +1,21 @@
+
+
+class Hero(object):
+    def __init__(self, name):
+        self.name = name
+        self.heroHealth = 100
+        self.heroPower = 10
+        self.weapon = ["gladius"]
+
+# class Goblin(object):
+#     def __init__(self, name):
+#         self.name = name
+#         self.goblinHealth = 100
+#         self.goblinPower = 5
+
+#     def attack(self, enemy):
+#         self.enemy = Hero.attack(Goblin)
+
+
+#         if self.weapon = "gladius":
+#             attack += 10
